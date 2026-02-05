f56f609843e43ace6a2e5af0909024c4d88a3316
Alesandro Maurini <maurini.alessandro.23@itisriva.edu.it>
2026-02-05 16:09:39 +0100

    Add files via upload

---
+++ esercizio1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crea un programma che carichi un vettore con N numeri e stampi quanti numeri uguali consecutivi ci sono.
+            Console.WriteLine($"scrivi un numero");
+            int n = int.Parse(Console.ReadLine());
+            int[]vettore = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Scrivi il {i+1}' numero");
+                vettore[i] = int.Parse(Console.ReadLine());
+            }
+
+            int consecutivi = 1;           
+            int massimoConsecutivi = 1;    
+
+            for (int i = 1; i < n; i++)
+            {
+                if (vettore[i] == vettore[i - 1])
+                {
+                    consecutivi++;
+                    if (consecutivi > massimoConsecutivi)
+                    {
+                        massimoConsecutivi = consecutivi;
+                    }
+                }
+                else
+                {
+                    consecutivi = 1;   
+                }
+            }
+
+            Console.WriteLine($"Il massimo di numeri uguali consecutivi è: {massimoConsecutivi}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ esercizio2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Carica un vettore di numeri.Stampa i numeri che si trovano in posizioni dispari e la loro somma e poi stampa i numeri che si trovano in posizioni pari e la loro somma.
+            Console.WriteLine($"Scrivi un numero");
+            int n = int.Parse(Console.ReadLine());
+            int[] vettore = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Scrivi il {i+1}' numero");
+                vettore[i] = int.Parse(Console.ReadLine());
+            }
+            int dispari = 0;
+            int pari = 0; 
+            for (int i = 0;i < vettore.Length; i++)
+            {
+                if (vettore[i] % 2 == 1)
+                {
+                    dispari = dispari + vettore[i];
+                }
+                else if (vettore[i] % 2 == 0)
+                {
+                    pari = pari + vettore[i];
+                }
+            }
+            Console.WriteLine($"la somma dei numeri pari e': {pari}");
+            Console.WriteLine($"la somma dei numeri dispari e': {dispari}");
+
+            Console.ReadKey();
+        }
+            
+    }
+}
